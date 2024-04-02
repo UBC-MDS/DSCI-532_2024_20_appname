fe3f9e7b099980dcd84548c57844c0c1b7a822ee
Farrandi Hernando <farrandi19@gmail.com>
2024-04-02 14:51:32 -0700

    made simple mvp for dashboard (#16)
    * some eda to get rough code
    
    * create helper function for plotting
    
    * add slider + dash plot
    
    * add docstring
    
    * add world map
    
    * add top emitters
    
    * some eda in nb
    
    * some wip

--- src/app.py
+++ src/app.py
@@ -1,21 +1,126 @@
-from dash import Dash, html, dash_table, dcc
+from dash import Dash, html, dash_table, dcc, Input, Output, callback
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
+import hotspot_plot as hp
+import dash_vega_components as dvc
+import json
 
 # Load data
-df = pd.read_csv('data/raw/owid-co2-data.csv')
-df = df[['country', 'year', 'population', 'gdp']]
-df = df.dropna()
+df = pd.read_csv("data/processed/co2-data.csv")
+with open("data/processed/country_codes.json", encoding="utf-8") as f:
+    country_codes = json.load(f)
 
 # Initialize Dash app
-app = Dash(__name__, title='Hotspot', external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, title="Hotspot", external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = dbc.Container([
-    html.Header('Simple Dash App: Hotspot', className='display-4'),
-    dash_table.DataTable(data=df.head(10).to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.scatter(df.head(10), x='gdp', y='population', color='country', size='year'))
-])
+app.layout = dbc.Container(
+    [
+        html.H1(children="Hotspot"),
+        html.Hr(),
+        dbc.Container(
+            [
+                html.H2("Filter Options"),
+                html.H4("Select Year Range"),
+                dcc.RangeSlider(
+                    min=1850,
+                    max=2022,
+                    step=1,
+                    value=[1850, 2022],
+                    marks={
+                        1850: "1850",
+                        1900: "1900",
+                        1950: "1950",
+                        2000: "2000",
+                        2022: "2022",
+                    },
+                    tooltip={"placement": "bottom"},
+                    pushable=20,
+                    id="year-slider",
+                    updatemode="drag",
+                ),
+                html.H4("Select Countries"),
+                dcc.Dropdown(
+                    id="country-dropdown",
+                    options=[
+                        # country_codes
+                        {"label": name, "value": code}
+                        for name, code in country_codes.items()
+                    ],
+                    multi=True,
+                ),
+            ]
+        ),
+        dbc.Container(
+            [
+                html.H2("World Map of CO2 Emissions"),
+                dcc.Graph(figure={}, id="world-map"),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.H3("Temperature and CO2 Emissions over Time"),
+                        dvc.Vega(
+                            id="global-temp-co2",
+                            opt={"renderer": "svg", "actions": False},
+                        ),
+                    ],
+                    width=7,
+                ),
+                dbc.Col(
+                    [
+                        html.H3("Top CO2 Emitters"),
+                        dvc.Vega(
+                            id="top-emmitters",
+                            opt={"renderer": "svg", "actions": False},
+                        ),
+                    ]
+                ),
+            ],
+            justify="center",
+        ),
+    ]
+)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+# Controls for Interactive Plot
+@callback(
+    Output("world-map", "figure"),
+    Input("year-slider", "value"),
+    Input("country-dropdown", "value"),
+)
+def update_world_map(year, country):
+    """
+    Update the world map based on the year range and selected countries.
+    """
+    return hp.plot_world_map(df, country, start_year=year[0], end_year=year[1])
+
+
+@callback(
+    Output("global-temp-co2", "spec"),
+    Input("year-slider", "value"),
+    Input("country-dropdown", "value"),
+)
+def update_global_temp_co2(year, country):
+    """
+    Update the global temperature and CO2 plot based on the year range selected.
+    """
+    return hp.plot_global_temp_co2(df, country, start_year=year[0], end_year=year[1])
+
+
+@callback(
+    Output("top-emmitters", "spec"),
+    Input("year-slider", "value"),
+    Input("country-dropdown", "value"),
+)
+def update_top_emitters(year, country):
+    """
+    Update the top CO2 emitters plot based on the year range selected.
+    """
+    return hp.plot_top_emitters(df, country, start_year=year[0], end_year=year[1])
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
---
+++ src/hotspot_plot.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import altair as alt
+import numpy as np
+import plotly.express as px
+
+
+def plot_global_temp_co2(df, country_codes, start_year=1850, end_year=2022):
+    """
+    Plots the global temperature and CO2 concentration from start_year to end_year.
+    """
+    co2_color = "red"
+    temp_color = "blue"
+
+    if country_codes:
+        df = df[df.iso_code.isin(country_codes)]
+    else:
+        df = df[df.country == "World"]
+
+    df_year = (
+        df.query(f"{start_year} <= year <= {end_year}")
+        .groupby("year")
+        .aggregate({"co2": "sum", "temperature_change_from_co2": "mean"})
+        .reset_index()
+        .dropna()
+    )
+
+    base = alt.Chart(df_year).encode(
+        alt.X("year:O", title="Year").axis(
+            labelAngle=0, values=list(np.linspace(start_year, end_year, 20).astype(int))
+        )
+    )
+
+    co2_line = base.mark_line(color=co2_color).encode(
+        y=alt.Y("co2").title("CO2 Emissions"),
+        tooltip=[alt.Tooltip("year:O"), alt.Tooltip("co2", title="CO2 Emissions")],
+    )
+    temp_line = base.mark_line(stroke=temp_color).encode(
+        y=alt.Y("temperature_change_from_co2").title("Temperature Change"),
+        tooltip=[
+            alt.Tooltip("year:O"),
+            alt.Tooltip("temperature_change_from_co2", title="Temperature Change"),
+        ],
+    )
+
+    return (
+        alt.layer(co2_line, temp_line)
+        .resolve_scale(y="independent")
+        .properties(
+            width=600,  # TODO: Change width to be responsive
+            height=400,
+        )
+        .configure_axisLeft(titleColor=co2_color, titleFontSize=12)
+        .configure_axisRight(titleColor=temp_color, titleFontSize=12)
+        .configure_title(fontSize=20)
+    ).to_dict()
+
+
+def plot_world_map(df, country_codes, start_year=1850, end_year=2022):
+    """
+    Plots the world map of CO2 emissions for the selected countries from start_year to end_year.
+    """
+
+    df_filtered = df.query(f"{start_year} <= year <= {end_year}")
+    if country_codes:
+        df_filtered = df_filtered[df_filtered.iso_code.isin(country_codes)]
+
+    fig = px.choropleth(
+        df_filtered,
+        locations="iso_code",
+        color="co2",
+        color_continuous_scale="reds",
+        labels={"co2": "CO2 Emissions"},
+        hover_name="country",
+        scope="world",
+    )
+    fig.update_layout(
+        margin={"r": 0, "t": 25, "l": 0, "b": 0},
+    )
+    fig.update_geos(
+        showcountries=True,
+        showland=True,
+        landcolor="lightgrey",
+        countrycolor="darkgrey",
+    )
+
+    return fig
+
+
+def plot_top_emitters(df, country_codes, start_year=1850, end_year=2022, n=10):
+    """
+    Plots the top n CO2 emitters from start_year to end_year.
+    """
+    df_filtered = df.query(f"{start_year} <= year <= {end_year}")
+    if country_codes:
+        df_filtered = df_filtered[df_filtered.iso_code.isin(country_codes)]
+
+    df_sorted = (
+        df_filtered.groupby("country")
+        .sum()
+        .sort_values("co2", ascending=False)
+        .head(n)
+        .reset_index()
+    )
+    return (
+        alt.Chart(df_sorted)
+        .mark_bar()
+        .encode(
+            y=alt.Y("country", title="Country").sort("-x"),
+            x=alt.X("co2", title="CO2 Emissions"),
+            color=alt.Color("country", legend=None),
+        )
+        .to_dict()
+    )
