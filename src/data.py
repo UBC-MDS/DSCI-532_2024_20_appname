0a9b33f03f9bd5a4d436cb03d7bea98040d63a16
Farrandi Hernando <farrandi19@gmail.com>
2024-04-17 10:18:11 -0700

    read from parquet instead of csv (#71)

--- src/data.py
+++ src/data.py
@@ -1,7 +1,8 @@
 import json
 import pandas as pd
 
-df = pd.read_csv("data/processed/co2-data.csv")
+# Get data from parquet file
+df = pd.read_parquet("data/processed/co2-data.parquet")
 
 with open("data/processed/country_codes.json", encoding="utf-8") as f:
     country_codes = json.load(f)
